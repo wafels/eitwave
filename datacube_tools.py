ef2187231df63f1b459e90fe2b594030b48f28dc
wafels <jackireland@gmail.com>
2014-11-16 12:06:38 -0500

    removed misnamed file

---
+++ datacube_tools.py
@@ -0,0 +1,27 @@
+#
+# Tools that implement common datacube operations.  The first argument in each
+# function is a numpy ndarray.  Datacubes are not the same as mapcubes.  
+#
+
+
+# Decorator testing the input for these functions
+def datacube_input(func):
+    def check(*args, **kwargs):
+        if not(isinstance(args[0], np.ndarray)):
+            raise ValueError('First argument must be a numpy ndarray.')
+
+        return func(*args, **kwargs)
+    return check
+
+
+@datacube_input
+def persistence(dc, func=np.max, axis=2):
+    """
+    Take an input datacube and return the persistance cube.
+    """
+    dc_persistance = np.zeros_like(dc)
+    dc_persistance[:, :, 0] = dc[:, :, 0]
+    for i in range(1, dc.shape[2]):
+        dc_persistance[:, :, i] = func(dc[:, :, 0: i + 1], axis=axis)
+
+    return dc_persistance
---
+++ mapcube_tools.py
@@ -0,0 +1,142 @@
+#
+# Tools that implement mapcube operations 
+#
+import numpy as np
+from sunpy.map import MapCube
+
+
+# Decorator testing the input for these functions
+def mapcube_input(func):
+    def check(*args, **kwargs):
+        if not(isinstance(args[0], MapCube)):
+            raise ValueError('First argument must be a sunpy MapCube.')
+    
+        if not(args[0].all_maps_same_shape()):
+            raise ValueError('All maps in the input mapcube must have the same shape.')
+
+        return func(*args, **kwargs)
+    return check
+
+
+@mapcube_input
+def data(mc):
+    """
+    Take an input mapcube and return a three-dimensional numpy array - a
+    datacube.  Chickens go in, pies come out.
+
+    Parameters
+    ----------
+    mc : sunpy.map.MapCube
+       A sunpy mapcube object
+
+    Returns
+    -------
+    ndarray
+       A numpy ndarray of dimension (ny, nx, nt).
+    """
+    nt = len(mc.maps)
+    shape = mc[0].data.shape
+    ny = shape[0]
+    nx = shape[1]
+    dc = np.zeros((ny, nx, nt))
+    for i in range(0, nt):
+        dc[:, :, i] = mc[i].data
+    return dc
+
+
+@mapcube_input
+def running_difference(mc, offset=1, use_offset_for_meta=True):
+    """
+    Calculate the running difference of a mapcube.
+
+    Parameters
+    ----------
+    mc : sunpy.map.MapCube
+       A sunpy mapcube object
+
+    offset : [ int ]
+       Calculate the running difference between map 'i + offset' and image 'i'.
+    use_offset_for_meta : boolean
+       Which meta header to use in layer 'i' in the returned mapcube, either
+       from map 'i + offset' (when set to True) and image 'i' (when set to
+       False).
+
+    Returns
+    -------
+    sunpy.map.MapCube
+       A mapcube containing the running difference of the input mapcube.
+    
+    """
+
+    # Create a list containing the data for the new map object
+    newmc = []
+    for i in range(0, len(mc.maps) - offset):
+        newdata = mc.maps.data[i + offset] - mc.maps.data[i]
+        if use_offset_for_meta:
+            newmeta = mc.maps.meta[i + offset]
+        else:
+            newmeta = mc.maps.meta[i]
+        newmc.append(Map(newdata, newmeta))
+
+    # Create the new mapcube and return
+    return Map(newmc, cube=True)
+
+
+@mapcube_input
+def persistence(mc, func=np.max):
+    """
+    Parameters
+    ----------
+    mc : sunpy.map.MapCube
+       A sunpy mapcube object
+
+    Returns
+    -------
+    sunpy.map.MapCube
+       A mapcube containing the running difference of the input mapcube.
+
+    """
+
+    # Get the persistence
+    persistence = datacube_tools.persistence(mc.as_array(), func=func)
+
+    # Create a list containing the data for the new map object
+    newmc = []
+    for i in range(0, len(mc.maps)):
+        newmc.append(Map(persistence[:, :, i], mc[i].meta))
+
+    # Create the new mapcube and return
+    return Map(newmc, cube=True)
+
+
+# Apply an arbitary function to every map in the mapcube and return a mapcube
+@mapcube_input
+def apply_to_each_map(mc, func, *args, **kwargs):
+    """
+    Take an input mapcube and apply a function to the data in each map, and
+    return a mapcube.
+
+    Parameters
+    ----------
+    mc : sunpy.map.MapCube
+       A sunpy mapcube object
+
+    func: Python function to be applied to the data in each map.  We assume
+    that the function can by applied as func(data, *args, **kwargs)
+
+    *args : arbitrary arguments
+    
+    **kwargs : arbitrary keyword arguments
+
+    Returns
+    -------
+    sunpy.map.MapCube
+       A sunpy mapcube object with maps in the same order as the input.
+
+    """
+    newmc = []
+    for m in mc:
+        newdata = func(m.data, *args, **kwargs)
+        newmc.append(Map(newdata, m.meta))
+
+    return Map(newmc, cube=True)
