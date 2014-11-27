might have written the dynamics calculation function incorrectly

aware.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 from sunpy.map import Map
+from sunpy.time import parse_time
 from skimage.morphology import closing, disk
 from skimage.filter.rank import median
 import mapcube_tools
@@ -35,6 +36,15 @@ def get_trigger_events(eventname):
         pkl_file.close()
 
 
+#
+# Some potential improvements
+#
+# 1. Do the median filtering and the closing on multiple length-scales
+#    Could add up the results at multiple length-scales to get a better idea of where the wavefront is
+#
+# 2. Apply the median and morphological operations on the 3 dimensional datacube, to take advantage
+#    of previous and future observations.
+#
 def processing(mc, median_radius=11, closing_radius=11, spike_level=25, accum=1):
     """
     Image processing steps used to isolate the EUV wave from the data.  Use
@@ -100,8 +110,54 @@ def dynamics(unraveled, params):
     Measurement of the progress of the wave across the disk.  This part of
     AWARE generates information concerning the dynamics of the wavefront.
     """
-    
+    # Get the times of the images
+    start_time = unraveled[0].date
+    times = np.asarray([(parse_time(m.date) - start_time).seconds for m in unraveled])
 
+    # Get the data
+    data = unraveled.as_array()
+
+    # At all times get an average location of the wavefront
+    latitude = np.min(closing_cleaned[10].yrange) + np.arange(0, dfinal.shape[1]) * params.get('lat_bin')
+    nlon = len(longitude)
+    nt = len(times)
+    results = []
+    for lon in range(0, nlon):
+        results_at_this_time = []
+        for i in range(0, nt):
+            emission = data[i, :, lon]
+            summed_emission = np.sum(emission)
+            # Simple estimate of where the bulk of the wavefront is
+            thisloc = np.sum(emission * latitude) / summed_emission
+            std = np.std(emission * latitude) / summed_emission
+
+            # Do a quadratic fit to the data
+            # Find where the location is defined
+            defined = np.isfinite(thisloc)
+            if len(defined) > 0:
+                # Get the times where the location is defined
+                timef = times[defined]
+                # Get the locations where the location is defined
+                locf = thisloc[defined]
+                # Get the locations relative to the first position
+                locf = np.abs(locf - locf[0])
+                # Get the standard deviation where the location is defined
+                stdf = std[defined]
+                # Do the quadratic fit to the data
+                quadfit = np.polyfit(timef, locf, 2, w=stdf)
+                # Calculate the best fit line
+                bestfit = np.polyval(quadfit, timef)
+                # create a dictionary that stores the results and append it
+                answer = {"bestfit": bestfit, "quadfit": quadfit,
+                                             "stdf": stdf, "locf": locf,
+                                             "timef": timef}
+            else:
+                answer = None
+            # Store the results at this time
+            results_at_this_time.append(answer)
+        # Store the collated results
+        results.append(results_at_this_time)
+    return results
 
 def write_movie(mc, filename, start=0, end=None):
     """

test_new_algorithm2.py
@@ -78,8 +78,9 @@ transformed = AWARE.processing(mc)
 # Get the location of the source event
 params = aware_utils.params(result[info[example]['result']])
 
-# Unravel the data
-umc = AWARE.unravel(transformed, params)
+# Unravel the data.  Note that the first element of the transformed array is, in these examples at least, not a good
+# representation of the wavefront.  It is there removed when calculating the unraveled maps
+umc = AWARE.unravel(transformed[1:], params)
 
 # Get the dynamics of the wave front
 dynamics = AWARE.dynamics(umc, params)
@@ -96,10 +97,6 @@ dynamics = AWARE.dynamics(umc, params)
 #visualize_dc(rdc3)
 #visualize(prdc3, draw_limb=True, draw_grid=True)
 
-
-"""
-
-"""
 
 
 
@@ -159,71 +156,8 @@ for i in range(0, nt):
     loc[i] = np.sum(dfinal[i, :, lon_index] * latitude) / np.sum(dfinal[i, :, lon_index])
     std[i] = np.std(dfinal[i, :, lon_index] * latitude / np.sum(dfinal[i, :, lon_index]))
 
-# Do a quadratic fit to the data
-isfinite = np.isfinite(loc)
-time = timescale * np.arange(0, nt)
-timef = time[isfinite][1:]
-locf = loc[isfinite][1:]
-locf = np.abs(locf - locf[0])
-stdf = std[isfinite][1:]
-quadfit = np.polyfit(timef, locf, 2, w=stdf)
-bestfit = np.polyval(quadfit, timef)
 
 factor = 1.21e4 # circumference of the Sun divided by its radius.
 vel = round(quadfit[1] * factor, 1)
 acc = round(quadfit[0] * factor, 1)
 
-plt.figure(3)
-plt.errorbar(timef, locf, yerr=stdf, fmt='go', label='measured wavefront position')
-plt.plot(timef, bestfit, label='quadratic fit')
-plt.title('wavefront position')
-plt.xlabel('elapsed time (seconds) after ' + mc[0].date)
-plt.ylabel('degrees traversed relative to launch site')
-xpos = 0.65 * np.max(timef)
-ypos = [np.min(locf) + 0.6 * (np.max(locf) - np.min(locf)), np.min(locf) + 0.7 * (np.max(locf) - np.min(locf))]
-plt.annotate(r'v = '+ str(vel) + ' $km/s$', [xpos, ypos[0]], fontsize=20)
-plt.annotate(r'a = '+ str(acc) + ' $km/s^{2}$', [xpos, ypos[1]], fontsize=20)
-plt.ylim(0, 1.3 * np.max(locf))
-plt.legend()
-
-
-
-
-
-
-# Convert this in to a datacube and get the cross sections out.  Super simple
-# to fit with Gaussians.  Note what we are measuring - it is the location of
-# what lit up next in the corona.  The actual bright front could be a lot
-# wider.
-
-"""
-get rid of salt and pepper noise now.
-
-From 
-
-http://scikit-image.org/docs/dev/auto_examples/applications/plot_morphology.html#example-applications-plot-morphology-py
-
-from skimage.filter.rank import median
-
-i = 7
-z = rdc3[:, :, i] / rdc3[:, :, i].max()
-plt.imshow(opening(z, disk(3)))
-plt.imshow(median(z, disk(11)))
-
-plt.imshow(closing(median(rdc3[:,:,i]/rdc3.max(), disk(11)),disk(11)))
-
-median filtering : from skimage.filter.rank import median
-bilateral filtering
-
-http://docs.opencv.org/trunk/doc/py_tutorials/py_imgproc/py_filtering/py_filtering.html
-
-"""
-
-
-#
-# After this, perhaps we can do some morphological operations to join
-# neighbouring weak pixels.  Perhaps also do a 3-d morphology to take
-# advantage of previous and future observations.
-#
-
-"""