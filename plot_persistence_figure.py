6ac4c6f9e77001fafd6200bcb396a2b6f2c8f415
Jack Ireland <jackireland@gmail.com>
2014-12-05 10:26:51 -0500

    added in a file that generates a plot that explains the persistence transform

---
+++ plot_persistence_figure.py
@@ -0,0 +1,45 @@
+#
+# Generate an plot that explains the persistence transform
+#
+import numpy as np
+import datacube_tools
+import matplotlib.pyplot as plt
+
+plt.ion()
+nt = 300
+dt = 12
+t = 12 * np.arange(0, nt)
+
+# Oscillation
+osc_amp = 1.0
+osc_per = 300.0
+
+# Linear amplitude
+lin_amp = 4.0
+
+# Noise level
+noise_amp = 1.5
+
+osc = 1.0 * np.sin(2 * np.pi * t / osc_per)
+
+linear = lin_amp * t / np.max(t)
+
+noise = noise_amp * np.random.normal(size=nt)
+
+data = osc + linear + noise
+
+npdata = np.zeros((1, 1, nt))
+npdata[0, 0, :] = data[:]
+
+persistence = datacube_tools.persistence(npdata)
+
+
+# Make the figure
+plt.plot(t, data, label='$f(t)$')
+plt.plot(t, persistence[0,0,:], label='$P(f(t))$')
+plt.legend(loc=4)
+plt.xlabel('time (seconds)')
+plt.ylabel('data (arbitrary units)')
+plt.show()
+
+
