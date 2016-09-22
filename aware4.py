fixed image scaling of running difference and base difference map cubes, enabling movies to be made

aware4.py
@@ -9,8 +9,6 @@ from copy import deepcopy
 import numpy as np
 import numpy.ma as ma
 import numpy.linalg as LA
-from scipy.ndimage.filters import median_filter
-from scipy.ndimage import grey_closing
 from scipy.signal import savgol_filter
 from scipy.optimize import minimize
 from skimage.morphology import disk
@@ -48,13 +46,39 @@ solar_circumference_per_degree_in_km = aware_constants.solar_circumference_per_d
 # 2. Apply the median and morphological operations on the 3 dimensional
 #    datacube, to take advantage of previous and future observations.
 #
+
+
+def apply_operations(mc, operations):
+    new = deepcopy(mc)
+    for operation in operations:
+        operation_function = operation.function
+        operation_kwargs = operation.kwargs
+        new = operation_function(new, operation_kwargs)
+
+    return new
+
+
+def apply_scaling(datacube, clip_limit, histogram_clip):
+    datacube[datacube < 0.0] = 0.0
+
+    # Clip the data to be within a range, and then normalize it.
+    if clip_limit is None:
+        cl = np.nanpercentile(datacube, histogram_clip)
+    datacube[datacube > cl[1]] = cl[1]
+    datacube = (datacube - cl[0]) / (cl[1]-cl[0])
+
+    # Get rid of NaNs
+    nans_here = np.logical_not(np.isfinite(datacube))
+    nans_replaced = deepcopy(datacube)
+    nans_replaced[nans_here] = 0.0
+
+    return nans_replaced, nans_here
+
+
 @mapcube_tools.mapcube_input
-def processing(mc, radii=[[11, 11]*u.degree],
+def processing(mc, mc_ops, cleaning_ops,
                clip_limit=None,
-               histogram_clip=[0.0, 99.],
-               func=np.sqrt,
-               develop=False,
-               verbose=True):
+               histogram_clip=[0.0, 99.]):
     """
     Image processing steps used to isolate the EUV wave from the data.  Use
     this part of AWARE to perform the image processing steps that segment
@@ -70,74 +94,30 @@ def processing(mc, radii=[[11, 11]*u.degree],
     func
     """
 
-    # Define the disks that will be used on all the images.
-    # The first disk in each pair is the disk that is used by the median
-    # filter.  The second disk is used by the morphological closing
-    # operation.
-    disks = []
-    for r in radii:
-        e1 = (r[0]/mc[0].scale.x).to('pixel').value  # median ellipse width - across wavefront
-        e2 = (r[0]/mc[0].scale.y).to('pixel').value  # median ellipse height - along wavefront
+    #
+    # Apply mapcube operations
+    #
+    #mc_ops = [Operation(mapcube_tools.persistence, None),
+    #          Operation(mapcube_tools.running_difference, None)]
+    new = apply_operations(mc, mc_ops)
 
-        e3 = (r[1]/mc[0].scale.x).to('pixel').value  # closing ellipse width - across wavefront
-        e4 = (r[1]/mc[0].scale.y).to('pixel').value  # closing ellipse height - along wavefront
+    #
+    # Get the scaled data out
+    #
+    scaled_data, nans_here = apply_scaling(new.as_array(), clip_limit, histogram_clip)
 
-        disks.append([disk(e1), disk(e3)])
+    # Clean the data on multiple lengthscales to isolate the wave front.
+    # Use three dimensional operations from scipy.ndimage.  This approach
+    # should get rid of more noise and have better continuity in the
+    # time-direction.
+    final = np.zeros_like(scaled_data, dtype=np.float32)
+    for cleaning_on_this_lengthscale in enumerate(cleaning_ops):
+        final += 1.0*apply_operations(scaled_data, cleaning_on_this_lengthscale)
 
-    # For the dump images
-    rstring = ''
-    for r in radii:
-        z = '%i_%i__' % (r[0].value, r[1].value)
-        rstring += z
-
-    # Calculate the persistence
-    new = mapcube_tools.persistence(mc)
-    if develop:
-        aware_utils.dump_images(new, rstring, '%s_1_persistence' % rstring)
-
-    # Calculate the running difference
-    new = mapcube_tools.running_difference(new)
-    if develop:
-        aware_utils.dump_images(new, rstring, '%s_2_rdiff' % rstring)
-
+    #
     # Storage for the processed mapcube.
+    #
     new_mc = []
-
-    # Only want positive differences, so everything lower than zero
-    # should be set to zero
-    mc_data = func(new.as_array())
-    mc_data[mc_data < 0.0] = 0.0
-
-    # Clip the data to be within a range, and then normalize it.
-    if clip_limit is None:
-        cl = np.nanpercentile(mc_data, histogram_clip)
-    mc_data[mc_data > cl[1]] = cl[1]
-    mc_data = (mc_data - cl[0]) / (cl[1]-cl[0])
-
-    # Get rid of NaNs
-    nans_here = np.logical_not(np.isfinite(mc_data))
-    nans_replaced = deepcopy(mc_data)
-    nans_replaced[nans_here] = 0.0
-
-    # Clean the data to isolate the wave front.  Use three dimensional
-    # operations from scipy.ndimage.  This approach should get rid of
-    # more noise and have better continuity in the time-direction.
-    final = np.zeros_like(mc_data, dtype=np.float32)
-
-    # Do the cleaning and isolation operations on multiple length-scales,
-    # and add up the final results.
-    for j, d in enumerate(disks):
-        superd = np.swapaxes(np.tile(d[0], (3, 1, 1)), 0, -1)
-        nr = deepcopy(nans_replaced)
-        print(' ')
-        print(nr.shape, superd.shape)
-        print('started median filter')
-        nr = 1.0*median_filter(nr, footprint=superd)
-        print('started grey closing')
-        nr = 1.0*grey_closing(nr, footprint=superd)
-
-        final += nr*1.0
-
     for i, m in enumerate(new):
         new_mc.append(Map(ma.masked_array(final[:, :, i],
                                           mask=nans_here[:, :, i]),

mapcube_tools.py
@@ -5,6 +5,9 @@ from copy import deepcopy
 import datetime
 import numpy as np
 import astropy.units as u
+from astropy.visualization import LinearStretch, PercentileInterval
+from astropy.visualization.mpl_normalize import ImageNormalize
+
 from sunpy.map.mapbase import GenericMap
 from sunpy.map import Map
 from sunpy.map import MapCube
@@ -42,6 +45,37 @@ def _mean_time(time_list):
     return base_time + datetime.timedelta(seconds=delta_t.value)
 
 
+#
+# Running difference
+#
+def data_cube_running_difference(dc, offset=1):
+    """
+    Calculate the running difference of a datacube.
+
+    Parameters
+    ----------
+    dc : three dimensional numpy array where the first two dimensions are
+         space.
+
+    offset : [ int ]
+       Calculate the running difference between map 'i + offset' and image 'i'.
+
+    Returns
+    -------
+    three dimensional numpy array
+       A datacube containing the running difference of the input datacube.
+
+    """
+    ny = dc.shape[0]
+    nx = dc.shape[1]
+    nt = dc.shape[2]
+    new_datacube = np.zeros((ny, nx, nt-offset))
+    for i in range(0, nt-offset):
+        new_datacube[:, :, i] = dc[:, :, i + offset] - dc[:, :, i]
+
+    return new_datacube
+
+
 @mapcube_input
 def running_difference(mc, offset=1, use_offset_for_meta='mean'):
     """
@@ -68,10 +102,15 @@ def running_difference(mc, offset=1, use_offset_for_meta='mean'):
 
     """
 
+    # Get the running difference of the data
+    new_datacube = data_cube_running_difference(mc.as_array(), offset=offset)
+
+    # These values are used to scale the images
+    vmin, vmax = PercentileInterval(99.0).get_limits(new_datacube)
+
     # Create a list containing the data for the new map object
     new_mc = []
     for i in range(0, len(mc.maps) - offset):
-        new_data = mc[i + offset].data - mc[i].data
         if use_offset_for_meta == 'ahead':
             new_meta = mc[i + offset].meta
         elif use_offset_for_meta == 'behind':
@@ -82,10 +121,67 @@ def running_difference(mc, offset=1, use_offset_for_meta='mean'):
                                                parse_time(mc[i].date)])
         else:
             raise ValueError('The value of the keyword "use_offset_for_meta" has not been recognized.')
-        new_mc.append(Map(new_data, new_meta))
+
+        # Update the plot scaling.  The default here attempts to produce decent
+        # looking images
+        new_m = Map(new_datacube[:, :, i], new_meta)
+        new_m.plot_settings['norm'] = ImageNormalize(vmin=vmin, vmax=vmax, stretch=LinearStretch())
+        new_mc.append(new_m)
 
     # Create the new mapcube and return
     return Map(new_mc, cube=True)
+
+
+#
+# base difference
+#
+def data_cube_base_difference(dc, base=0, fraction=False):
+    """
+    Calculate the base difference of a datacube.
+
+    Parameters
+    ----------
+    dc : three dimensional numpy array where the first two dimensions are
+         space.
+
+    base : int, two-dimensional numpy array
+       If base is an integer, this is understood as an index to the input
+       mapcube.  Differences are calculated relative to the map at index
+       'base'.  If base is a sunpy map, then differences are calculated
+       relative to that map
+
+    fraction : boolean
+        If False, then absolute changes relative to the base map are
+        returned.  If True, then fractional changes relative to the base map
+        are returned
+
+    Returns
+    -------
+    numpy 3 dimensional array
+       A data cube containing base difference of the input data cube.
+
+    """
+
+    if not(isinstance(base, np.ndarray)):
+        base_data = dc[:, :, 0]
+    else:
+        base_data = base
+
+    if base_data.shape != dc.shape[0:2]:
+        raise ValueError('Base map does not have the same shape as the maps in the input datacube.')
+
+    # Fractional changes or absolute changes
+    if fraction:
+        relative = base_data
+    else:
+        relative = 1.0
+
+    # Create a list containing the data for the new map object
+    new_datacube = np.zeros_like(dc)
+    for i in range(0, dc.shape[2]):
+        new_datacube[:, :, i] = (dc[:, :, i] - base_data) / relative
+
+    return new_datacube
 
 
 @mapcube_input
@@ -124,17 +220,18 @@ def base_difference(mc, base=0, fraction=False):
     if base_data.shape != mc[0].data.shape:
         raise ValueError('Base map does not have the same shape as the maps in the input mapcube.')
 
-    # Fractional changes or absolute changes
-    if fraction:
-        relative = base_data
-    else:
-        relative = 1.0
+    # Get the base difference of the
+    new_datacube = data_cube_base_difference(mc.as_array(), base=base, fraction=fraction)
+
+    # These values are used to scale the images.
+    vmin, vmax = PercentileInterval(99.0).get_limits(new_datacube)
 
     # Create a list containing the data for the new map object
     new_mc = []
-    for m in mc:
-        new_data = (m.data - base_data) / relative
-        new_mc.append(Map(new_data, m.meta))
+    for i, m in enumerate(mc):
+        new_m = Map(new_datacube[:, :, i], m.meta)
+        new_m.plot_settings['norm'] = ImageNormalize(vmin=vmin, vmax=vmax, stretch=LinearStretch())
+        new_mc.append(new_m)
 
     # Create the new mapcube and return
     return Map(new_mc, cube=True)

swave_study.py
@@ -6,6 +6,8 @@
 import os
 import numpy as np
 import astropy.units as u
+from scipy.ndimage.filters import median_filter
+from scipy.ndimage import grey_closing
 
 observational = True
 
@@ -45,10 +47,10 @@ if not observational:
 
 else:
     # Which wave?
-    wave_name = 'longetal2014_figure4'  # June 7 2011
-    wave_name = 'longetal2014_figure7'  # 13 February 2011
-    wave_name = 'longetal2014_figure8a'  # 15 February 2011
-    wave_name = 'longetal2014_figure8e'  # 16 February 2011
+    #wave_name = 'longetal2014_figure4'  # June 7 2011
+    #wave_name = 'longetal2014_figure7'  # 13 February 2011
+    #wave_name = 'longetal2014_figure8a'  # 15 February 2011
+    #wave_name = 'longetal2014_figure8e'  # 16 February 2011
     #wave_name = 'longetal2014_figure6'  # 8 February 2011, no wave
     wave_name = 'byrneetal2013_figure12'  # 16 February 2011
 
@@ -88,10 +90,10 @@ if not observational:
     spatial_summing = [4, 4]*u.pix
 else:
     # Summing of the observations in the time direction
-    temporal_summing = 2
+    temporal_summing = 1
 
     # Summing of the observations in the spatial directions
-    spatial_summing = [1, 1]*u.pix
+    spatial_summing = [2, 2]*u.pix
 
 
 # Oversampling along the wavefront
@@ -110,10 +112,36 @@ transform_hpc2hg_parameters = {'lon_bin': 1.0*u.degree,
 
 # HPC to HG transformation: methods used to calculate the griddata
 # interpolation
-griddata_methods = ('linear', 'nearest')
+#griddata_methods = ('linear', 'nearest')
 griddata_methods = ('nearest',)
 
 
+# A simple class that holds operations and their keywords
+class Operation:
+    def __init__(self, function, kwargs):
+        self.function = function
+        self.kwargs = kwargs
+
+"""
+# Define the disks that will be used on all the images.
+# The first disk in each pair is the disk that is used by the median
+# filter.  The second disk is used by the morphological closing
+# operation.
+disks = []
+for r in radii:
+    e1 = (r[0] / mc[0].scale.x).to('pixel').value  # median ellipse width - across wavefront
+    e2 = (r[0] / mc[0].scale.y).to('pixel').value  # median ellipse height - along wavefront
+
+    e3 = (r[1] / mc[0].scale.x).to('pixel').value  # closing ellipse width - across wavefront
+    e4 = (r[1] / mc[0].scale.y).to('pixel').value  # closing ellipse height - along wavefront
+
+    disks.append([disk(e1), disk(e3)])
+
+cleaning_ops = [[Operation(median_filter, {"footprint": superd[1]}),
+                 Operation(grey_closing, {"footprint": superd[1]})],
+                [Operation(median_filter, {"footprint": superd[1]}),
+                 Operation(grey_closing, {"footprint": superd[1]})]]
+"""
 ###############################################################################
 #
 # AWARE processing: details