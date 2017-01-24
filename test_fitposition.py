4223f7e60d7dfb168286e372f635dc25ed0df0b9
Jack Ireland <jackireland@gmail.com>
2017-01-24 08:07:59 -0500

    latex inline units - much better

--- test_fitposition.py
+++ test_fitposition.py
@@ -6,11 +6,12 @@ from aware_constants import solar_circumference_per_degree
 
 nt = 20
 dt = 36.0*u.s
-sigma = 10*u.degree
+sigma = 5*u.degree
 
 t = dt*np.arange(0, nt)
 s0 = 0*u.degree
-v = ((400*u.km/u.s)/solar_circumference_per_degree).to(u.deg/u.s)
+v0 = 500*u.km/u.s
+v = (v0/solar_circumference_per_degree).to(u.deg/u.s)
 error = sigma*np.ones(nt)
 
 na = 40
@@ -54,22 +55,25 @@ a2 = np.mean(z2a, axis=1)
 a2e = np.std(z2a, axis=1)
 accs = (a * solar_circumference_per_degree).to(u.km/u.s/u.s)
 
+v_string = v0.unit.to_string('latex_inline')
+a_string = a0.unit.to_string('latex_inline')
 plt.ion()
 plt.close('all')
 plt.figure(1)
 plt.errorbar(accs.value, v1.value, yerr=v1e.value, label='degree 1, fit velocity')
 plt.errorbar(accs.value, v2.value, yerr=v2e.value, label='degree 2, fit velocity')
-plt.axhline((v*solar_circumference_per_degree).to(u.km/u.s).value, label='true velocity', color='r')
-plt.xlabel('true acceleration (km/s/s)')
-plt.ylabel('velocity (km/s)')
+plt.axhline((v*solar_circumference_per_degree).to(u.km/u.s).value,
+            label='true velocity ({:n} {:s})'.format(v0.value, v_string), color='r')
+plt.xlabel('true acceleration ({:s})'.format(a_string))
+plt.ylabel('velocity ({:s})'.format(v_string))
 plt.legend(framealpha=0.5, loc='upper left')
 plt.grid()
 
 plt.figure(2)
 plt.errorbar(accs.value, a2.value, yerr=a2e.value, label='degree 2, acceleration')
 plt.plot(accs.value, accs.value, label='true acceleration', color='r')
-plt.xlabel('true acceleration (km/s/s)')
-plt.ylabel('fit acceleration (km/s/s)')
+plt.xlabel('true acceleration ({:s})'.format(a_string))
+plt.ylabel('fit acceleration ({:s})'.format(a_string))
 plt.legend(framealpha=0.5, loc='upper left')
 plt.grid()
 
